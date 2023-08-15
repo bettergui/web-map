56d19131a75d5bc0884c1977d73a12435026c997
Vietanh Nguyen <vietanh@uber.com>
2023-08-15 13:31:36 -0700

    used uber style

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken =
 "pk.eyJ1IjoiYmV0dGVyZ3VpIiwiYSI6ImNrMDBrb2IzODBta2UzbXFzZXkxaGhsZnAifQ.L5f-wDyZLGX7sRPZ9IFK8A";
 const map = new mapboxgl.Map({
    container: "map", // container ID
-   style: "mapbox://styles/bettergui/cll465svt008c01ojex9obsh2", // style URL
+   style: "https://d3dt5tsgfu6lcf.cloudfront.net/style/santurce/web?mode=default", // style URL
    center: [-74.5, 40], // starting position [lng, lat]
    zoom: 9, // starting zoom
 });
